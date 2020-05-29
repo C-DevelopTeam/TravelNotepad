95b6fe9f4636e2f1fa0f0038fe32b42addce2eb7
Arthur-0 <1015982558@qq.com>
2020-05-29 14:50:26 +0800

    实体类

---
+++ TravelApi/models/site.cs
@@ -0,0 +1,18 @@
+namespace TravelApi.Models
+{
+    class site
+    {
+        [key]
+        public string siteid{get;set;}
+
+        public string sitename{get;set;}
+
+        public string distinct{get;set;}
+
+        public string adcode{get;set;}
+
+        public string location{get;set;}
+        
+        public string address{get;set;}
+    }
+}
